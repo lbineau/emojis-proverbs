3b3fb244b1f34f85846f3df6841f67150e429cc9
lbineau <lory.bineau@merkle.com>
2024-09-17 21:37:57 +0200

    ux enhancement

--- src/views/List.vue
+++ src/views/List.vue
@@ -9,11 +9,12 @@ const quotesTable = reactive(quotes)
 
 <template>
     <div class="view-list">
-        <DataTable :value="quotesTable">
-            <Column field="emojis" header="Emojis" bodyStyle="width: 30%;max-width:150px"></Column>
-            <Column field="quote" header="Expression">
+        <DataTable :value="quotesTable" stripedRows>
+            <Column field="emojis" header="Emojis" bodyStyle="font-size: 1.7rem"></Column>
+            <!--<Column field="quote" header="Expression"></Column>-->
+            <Column field="link" header="Lien">
                 <template #body="{ data }">
-                    <Button as="a" :href="data.link" :label="data.quote" link></Button>
+                    <Button as="a" :href="data.link" icon="pi pi-link" :label="data.quote" link></Button>
                 </template>
             </Column>
         </DataTable>
@@ -23,7 +24,28 @@ const quotesTable = reactive(quotes)
 <style scoped>
 .view-list {
     max-width: 100%;
+}
+
+.p-datatable-table-container {
+    padding: 1rem;
+}
+
+.view-list {
+    max-width: 100%;
+}
+
+.p-button {
     --p-button-padding-x: 0;
     --p-button-padding-y: 0;
+    --p-button-link-color: white;
+    text-decoration: none;
+    align-items: baseline;
+}
+
+@media (max-width: 640px) {
+    .view-list {
+        --p-datatable-header-cell-padding: 0.75rem 0.5rem;
+        --p-datatable-body-cell-padding: 0.75rem 0.5rem;
+    }
 }
 </style>
