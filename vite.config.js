79e3453036e48bb4ae52567f0545634a8848fb18
lbineau <lory.bineau@merkle.com>
2024-09-21 15:52:51 +0200

    add icons with @vite-pwa/assets-generator

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,49 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Expremojis',
+        short_name: 'Expremojis',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        theme_color: '#333333',
+        background_color: '#333333',
+        display: 'standalone'
+      }
+    })
   ],
   resolve: {
     alias: {
